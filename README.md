7335d3d8918ec5fcd8cd1147bd97adfbc808dc47
laynefaler <laynefaler@gmail.com>
2017-11-29 06:38:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# HTTPS Stack OverFlow Question
+
+## Question
+```
+
+```
+
+## To install & run
+    * Required node.js & Mac Users Only
+    * `bash ./setup.sh`
+    * `npm i`
+    * `npm start`
---
+++ app.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const Express = require('express');
+const fs = require('fs');
+
+
+const app = new Express();
+
+app.get('/', (req, res) => {
+	res.status(200).send('It works!');
+});
+
+const server = https.createServer({
+	key: fs.readFileSync('./key.pem'),
+	cert: fs.readFileSync('./certificate.pem'),
+}, app);
+
+server.listen(3000, () => {
+  console.log('Listen to https://localhost:3000');
+});
